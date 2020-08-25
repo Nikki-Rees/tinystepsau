a2b05eddf9e1eb3bc247088f5e68eb8e7b181791
Dan Swan <rodger.swan@gmail.com>
2020-08-25 19:36:35 +1000

    seed data

---
+++ db/seeds.sql
@@ -0,0 +1,52 @@
+INSERT INTO Habits
+    (type, activity, frequency, createdAt, updatedAt)
+VALUES
+    ("physical", "bike riding", "30 minutes of riding at an easy pace", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" );
+INSERT INTO Habits
+    (type, activity, frequency, createdAt, updatedAt)
+VALUES
+    ("physical", "hiking", "60 minutes on an easy track", "weekly", "2020-08-24 09:59:46", "2020-08-24 09:59:46" );
+INSERT INTO Habits
+    (type, activity, frequency, createdAt, updatedAt)
+VALUES
+    ("physical", "stetching", "10 minutes for entire body", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" );
+INSERT INTO Habits
+    (type, activity, frequency, createdAt, updatedAt)
+VALUES
+    ("physical", "weight lifting", "total body training", "3 times a week", "2020-08-24 09:59:46", "2020-08-24 09:59:46" );
+
+
+INSERT INTO Habits
+    (type, activity, frequency, createdAt, updatedAt)
+VALUES
+    ("mental", "gratitude journal", "For this technique, ask yourself a question, for example, 'What are you most grateful for?' Be aware of the feelings, not the thoughts, that arise when you focus on the question.", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" );
+INSERT INTO Habits
+    (type, activity, frequency, createdAt, updatedAt)
+VALUES
+    ("mental", "mindfulness - focused attention", "This technique uses the breath to anchor the mind and maintain awareness. Focus your attention on the breath — specifically the rise and fall of the chest — and return to the breath whenever you get distracted or notice your mind starting to wander.", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" );
+INSERT INTO Habits
+    (type, activity, frequency, createdAt, updatedAt)
+VALUES
+    ("mental", "mindfulness - visualization", "This technique uses visualization, to focus on a person or something more abstract, to hold attention. The idea here is that the familiar image will help create and maintain a relaxed focus.", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" );
+INSERT INTO Habits
+    (type, activity, frequency, createdAt, updatedAt)
+VALUES
+    ("mental", "mindfulness - body scan", "This technique, which uses meditation to connect with the body, involves scanning your body from head to toe and being aware of any discomfort, sensations, or aches that exist (which could be indicators of stress and anxiety", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" );
+
+
+INSERT INTO Habits
+    (type, activity, frequency, createdAt, updatedAt)
+VALUES
+    ("financial", "skip your morning coffee", "For the next month skip your usual morning coffee and save the extra $25", "every weekday", "2020-08-24 09:59:46", "2020-08-24 09:59:46" );
+INSERT INTO Habits
+    (type, activity, frequency, createdAt, updatedAt)
+VALUES
+    ("financial", "take lunch", "Instead of buying lunch out ", "every weekday", "2020-08-24 09:59:46", "2020-08-24 09:59:46" );
+INSERT INTO Habits
+    (type, activity, frequency, createdAt, updatedAt)
+VALUES
+    ("financial", "bike riding", "description", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" );
+INSERT INTO Habits
+    (type, activity, frequency, createdAt, updatedAt)
+VALUES
+    ("financial", "bike riding", "description", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" );
