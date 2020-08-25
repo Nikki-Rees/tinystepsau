c28d7f32999e3c4b1b8ba741290c64374f914a7b
Nikki-Rees <nikki-rees@outlook.com>
2020-08-25 20:12:59 +1000

    Sign up page link fix

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,8 +4,8 @@ const path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
-  app.get("/", (req, res) => {
+module.exports = function (app) {
+  app.get("/signup", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
