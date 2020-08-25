a9b43ad564b08d9db774cd7745d9b95f998835ac
Nikki-Rees <nikki-rees@outlook.com>
2020-08-25 21:47:15 +1000

    Handlebars files added

--- routes/habit-api-routes.js
+++ routes/habit-api-routes.js
@@ -4,9 +4,25 @@ const db = require("../models");
 // =============================================================
 module.exports = (app) => {
 
-    // GET route for getting all of the posts
+    // GET route for getting all of the habit options
     app.get("/api/habits", (req, res) => {
         const query = {};
         if (req.query.habit_id) {
             query.HabitId = req.query.habit_id;
         }
+
+
+        // POST route for saving a new todo
+        app.post("/api/habits", (req, res) => {
+            console.log(req.body);
+            // create takes an argument of an object describing the item we want to
+            // insert into our table. In this case we just we pass in an object with a text
+            // and complete property (req.body)
+            db.Todo.create({
+                text: req.body.text,
+                complete: req.body.complete
+            }).then((dbTodo) => {
+                // We have access to the new todo as an argument inside of the callback function
+                res.json(dbTodo);
+            });
+        });
