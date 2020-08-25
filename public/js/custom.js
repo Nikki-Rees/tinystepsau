c3b95dfc96ffa34874f87021338df6fa528e02ab
Anna C <anna.s.chong@gmail.com>
2020-08-25 19:51:02 +1000

    ADD custom js for page loading

---
+++ public/js/custom.js
@@ -0,0 +1,29 @@
+// Page loading animation
+$(window).on('load', function () {
+    if ($('.cover').length) {
+        $('.cover').parallax({
+            imageSrc: $('.cover').data('image'),
+            zIndex: '1'
+        });
+    }
+
+    $("#preloader").animate({
+        'opacity': '0'
+    }, 600, function () {
+        setTimeout(function () {
+            $("#preloader").css("visibility", "hidden").fadeOut();
+        }, 300);
+    });
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    var box = $('.header-text').height();
+    var header = $('header').height();
+
+    if (scroll >= box - header) {
+        $("header").addClass("background-header");
+    } else {
+        $("header").removeClass("background-header");
+    }
+});
