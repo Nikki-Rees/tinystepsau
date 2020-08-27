c09d53053338d218ba81eb58ec85e250e9518f74
Dan Swan <rodger.swan@gmail.com>
2020-08-27 22:04:20 +1000

    linter disable

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 const bcrypt = require("bcryptjs");
 
