b21778442e0f05bb3e979bd550d453f373d1799d
Dan Swan <rodger.swan@gmail.com>
2020-08-27 21:59:41 +1000

    config issues

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
-module.exports =
-{
-  "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": "lpb4g4j3d5puu9aw",
-    "host": "qn0cquuabmqczee2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    "dialect": "mysql"
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: 3306,
+    dialect: "mysql",
   },
-  "test": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": "lpb4g4j3d5puu9aw",
-    "host": "qn0cquuabmqczee2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    "dialect": "mysql"
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "lpb4g4j3d5puu9aw",
+    host: "qn0cquuabmqczee2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    dialect: "mysql",
   },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
-  }
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql",
+  },
 };
