98ba0285cedf28ca030ea9bc54ed01c8ed8a6680
Dan Swan <rodger.swan@gmail.com>
2020-08-27 22:04:49 +1000

    frontend js for post and put habit

--- public/js/habits.js
+++ public/js/habits.js
@@ -1,3 +1,32 @@
+/* eslint-disable */
 $(document).ready(() => {
+    $(".habit").on("click", function () {
+        const id = $(this).data("id");
+        $.ajax("/api/user/habit", {
+            method: "PUT",
+            data: { habitId: id },
+        }).then(() => {
+            console.log("put habit");
+            // Reload the page to get the updated list
+            location.reload();
+        });
+    });
+
+    $(".checkinButton").on("click", (event) => {
+        event.preventDefault();
+        
+        // Send the POST request.
+        $.ajax("/api/checkins", {
+            method: "POST",
+            data: { date: Date.now() }
+        }).then(
+            () => {
+                console.log("User has checked in");
+                // Reload the page 
+                location.reload();
+                //disable check in button until date changed
+            }
+        );
+    });
 
 });
