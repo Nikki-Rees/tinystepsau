57eacf81fd8ff49365cd88630f6d97e84f11ece7
Dan Swan <rodger.swan@gmail.com>
2020-08-29 14:55:10 +1000

    event listener works but not the ajax method

--- public/js/checkin.js
+++ public/js/checkin.js
@@ -1,18 +1,19 @@
 /* eslint-disable */
-$(".checkinButton").on("click", (event) => {
-  event.preventDefault();
-        
-  // Send the POST request.
-  $.ajax("/api/checkins", {
-    method: "POST",
-    data: { date: Date.now() }
-  }).then(
-    () => {
-      console.log("User has checked in");
-      // Reload the page 
-      location.reload();
-      //disable check in button until date changed
-    }
-  );
-});
+$(document).ready(() => {
+  //follow login.js and assign variables to match the form in checkin handlebars
 
+  $("#checkin-button").on("click", (event) => {
+    event.preventDefault();
+    
+    // Send the POST request.
+    $.ajax("/api/checkins", {
+      method: "POST",
+      data: { date: Date.now() },
+    }).then(() => {
+      console.log("User has checked in");
+      
+      //need login to disable check in button until date changed
+      // maybe display a handlebars that says "You've checked in your habit for today"
+    });
+  });
+});
