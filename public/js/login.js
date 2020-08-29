53f6cc8f0744a9f2836417a2274f88c9dd717772
Dan Swan <rodger.swan@gmail.com>
2020-08-29 14:55:35 +1000

    login page redirects to checkin

--- public/js/login.js
+++ public/js/login.js
@@ -29,7 +29,7 @@ $(document).ready(() => {
       password: password
     })
       .then(() => {
-        window.location.replace("/habits");
+        window.location.replace("/checkin");
         // If there's an error, log the error
       })
       .catch(err => {
