9a40fe2b3905f00c044eea62f19abed027ca5ed7
Dan Swan <rodger.swan@gmail.com>
2020-08-30 17:12:19 +1000

    create new habit form working both front and back

--- public/js/habits.js
+++ public/js/habits.js
@@ -1,15 +1,48 @@
 /* eslint-disable */
 $(document).ready(() => {
-    $(".habit").on("click", function () {
-        console.log("habit clicked");
-        const id = $(this).data("id");
-        $.ajax("/api/user/habit", {
-            method: "PUT",
-            data: { habitId: id },
-        }).then(() => {
-            console.log("put habit");
-            // Reload the page to get the updated list
-            location.replace("/checkin");
-        });
+  const createHabitForm = $("form#create-habit");
+  const typeInput = $("textarea#type");
+  const activityInput = $("textarea#activity");
+  const descriptionInput = $("textarea#description");
+  const frequencyInput = $("textarea#frequency");
+
+  createHabitForm.on("submit", (event) => {
+    event.preventDefault();
+    const newHabit = {
+      type: typeInput.val().trim(),
+      activity: activityInput.val().trim(),
+      description: descriptionInput.val().trim(),
+      frequency: frequencyInput.val().trim(),
+    };
+
+    if (
+      newHabit.type &&
+      newHabit.activity &&
+      newHabit.description &&
+      newHabit.frequency
+    ) {
+      createNewHabit(newHabit);
+    }
+  });
+
+  function createNewHabit(newHabit) {
+    $.post("/api/create-habit", {
+      type: newHabit.type,
+      activity: newHabit.activity,
+      description: newHabit.description,
+      frequency: newHabit.frequency,
+    }).then((data) => {
+        console.log(data);
+        window.location.replace("/checkin");
     });
+  }
 });
+
+// const id = $(this).data("id");
+// $.post("/api/user/habit", {
+//   method: "PUT",
+//   data: { habitId: id },
+// }).then(() => {
+//   // Reload the page to get the updated list
+//   window.location.replace("/checkin");
+// });
