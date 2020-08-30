a8d84945a05f1eb0edc064588094183d5a612595
Nikki-Rees <nikki-rees@outlook.com>
2020-08-30 13:10:44 +1000

    Calender code added

--- public/js/calender.js
+++ public/js/calender.js
@@ -1,39 +1,61 @@
+// function drawChart() {
+//    const dataTable = new google.visualization.DataTable();
+//    dataTable.addColumn({ type: 'date', id: 'Date' });
+//    dataTable.addColumn({ type: 'number', id: 'CheckedIn' });
+//    dataTable.addRows([
+//       [ new Date(2012, 3, 13), 37032 ],
+//       [ new Date(2012, 3, 14), 38024 ],
+//       [ new Date(2012, 3, 15), 38024 ],
+//       [ new Date(2012, 3, 16), 38108 ],
+//       [ new Date(2012, 3, 17), 38229 ],
+//       // Many rows omitted for brevity.
+//       [ new Date(2013, 9, 4), 38177 ],
+//       [ new Date(2013, 9, 5), 38705 ],
+//       [ new Date(2013, 9, 12), 38210 ],
+//       [ new Date(2013, 9, 13), 38029 ],
+//       [ new Date(2013, 9, 19), 38823 ],
+//       [ new Date(2013, 9, 23), 38345 ],
+//       [ new Date(2013, 9, 24), 38436 ],
+//       [ new Date(2013, 9, 30), 38447 ]
+//     ]);
 
-  google.charts.load("current", {packages:["calendar"]});
-  google.charts.setOnLoadCallback(drawChart);
+//    const chart = new google.visualization.Calendar(document.getElementById('calendar_basic'));
 
-function drawChart() {
-   const dataTable = new google.visualization.DataTable();
-   dataTable.addColumn({ type: 'date', id: 'Date' });
-   dataTable.addColumn({ type: 'number', id: 'CheckedIn' });
-   dataTable.addRows([
-      [ new Date(2012, 3, 13), 37032 ],
-      [ new Date(2012, 3, 14), 38024 ],
-      [ new Date(2012, 3, 15), 38024 ],
-      [ new Date(2012, 3, 16), 38108 ],
-      [ new Date(2012, 3, 17), 38229 ],
-      // Many rows omitted for brevity.
-      [ new Date(2013, 9, 4), 38177 ],
-      [ new Date(2013, 9, 5), 38705 ],
-      [ new Date(2013, 9, 12), 38210 ],
-      [ new Date(2013, 9, 13), 38029 ],
-      [ new Date(2013, 9, 19), 38823 ],
-      [ new Date(2013, 9, 23), 38345 ],
-      [ new Date(2013, 9, 24), 38436 ],
-      [ new Date(2013, 9, 30), 38447 ]
-    ]);
+//   const options = {
+//      title: "Progress",
+//      height: 350,
+//      noDataPattern: {
+//        backgroundColor: '#76a7fa',
+//        color: '#a0c3ff'
+//      }
+//    };
+//    };
 
-   const chart = new google.visualization.Calendar(document.getElementById('calendar_basic'));
+//    chart.draw(dataTable, options);
+// };
 
-  const options = {
-     title: "Progress",
-     height: 350,
-     noDataPattern: {
-       backgroundColor: '#76a7fa',
-       color: '#a0c3ff'
-     }
-   };
-   };
+    const googleCharts = require("https://www.gstatic.com/charts/loader.js");
+    const ajax = require("//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js");
+   
+    
+    // Load the Visualization API and the piechart package.
+    google.charts.load('current', {'packages':['calendar']});
+      
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+      
+    function drawChart() {
+      const jsonData = $.ajax({
+          url: "",
+          dataType: "json",
+          async: false
+          }).responseText;
+          
+      // Create our data table out of JSON data loaded from server.
+      const data = new google.visualization.DataTable(jsonData);
 
-   chart.draw(dataTable, options);
-};
+      // Instantiate and draw our chart, passing in some options.
+      const chart = new google.visualization.Calendar(document.getElementById('calendar_basic'));
+      chart.draw(data, {width: 400, height: 240});
+    };
+
