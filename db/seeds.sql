65c8028326fc1fdeee6c77b95c92312266e95f79
Nikki-Rees <nikki-rees@outlook.com>
2020-08-31 09:59:40 +1000

    Fix and update to seed file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,15 @@
 INSERT INTO Habits
-    (type, activity, frequency, createdAt, updatedAt)
+    (type, activity, description, frequency, createdAt, updatedAt)
 VALUES
-    ("physical", "bike riding", "30 minutes of riding at an easy pace", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
-    ("physical", "hiking", "60 minutes on an easy track", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
-    ("physical", "stetching", "10 minutes for entire body", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
-    ("physical", "weight lifting", "total body training", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
-    ("mental", "gratitude journal", "For this technique, ask yourself a question, for example, 'What are you most grateful for?' Be aware of the feelings, not the thoughts, that arise when you focus on the question.", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
-    ("mental", "mindfulness - focused attention", "This technique uses the breath to anchor the mind and maintain awareness. Focus your attention on the breath — specifically the rise and fall of the chest — and return to the breath whenever you get distracted or notice your mind starting to wander.", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
-    ("mental", "mindfulness - visualization", "This technique uses visualization, to focus on a person or something more abstract, to hold attention. The idea here is that the familiar image will help create and maintain a relaxed focus.", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
-    ("mental", "mindfulness - body scan", "This technique, which uses meditation to connect with the body, involves scanning your body from head to toe and being aware of any discomfort, sensations, or aches that exist (which could be indicators of stress and anxiety", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
-    ("financial", "skip your morning coffee", "For the next month skip your usual morning coffee and save the extra $25", "every weekday", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
-    ("financial", "take lunch", "Instead of buying lunch out ", "every weekday", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
-    ("financial", "track your spending", "keep a track of your daily spending", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" );
-    ("financial", "save $1 a day", "save $1 a day", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" );
+    ("Physical", "bike riding", "30 minutes of riding at an easy pace", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
+    ("Physical", "hiking", "60 minutes on an easy track", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
+    ("Physical", "stretching", "10 minutes for entire body", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
+    ("Physical", "weight lifting", "total body training", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
+    ("Mental", "gratitude journal", "For this technique, ask yourself a question, for example, 'What are you most grateful for?' Be aware of the feelings, not the thoughts, that arise when you focus on the question.", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
+    ("Mental", "mindfulness - focused attention", "This technique uses the breath to anchor the mind and maintain awareness. Focus your attention on the breath.", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
+    ("Mental", "mindfulness - visualization", "This technique uses visualization, to focus on a person or something more abstract, to hold attention.", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
+    ("Mental", "mindfulness - body scan", "This technique, which uses meditation to connect with the body, involves scanning your body from head to toe.", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
+    ("Financial", "skip your morning coffee", "For the next month skip your usual morning coffee and save the extra $25", "every weekday", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
+    ("Financial", "take lunch", "Instead of buying lunch out ", "every weekday", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
+    ("Financial", "track your spending", "keep a track of your daily spending", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" ),
+    ("Financial", "save $1 a day", "save $1 a day", "daily", "2020-08-24 09:59:46", "2020-08-24 09:59:46" );
