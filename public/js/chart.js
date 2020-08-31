2cb77a6a861410bb227000a63cb70085e5b6e7c3
Dan Swan <rodger.swan@gmail.com>
2020-08-31 17:40:34 +1000

    chart working with live data

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,16 +1,18 @@
 /* eslint-disable */
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-    type: 'pie',
+var ctx = document.getElementById("myChart");
+
+$.get("/api/checkins").then((response) => {  
+  var myChart = new Chart(ctx, {
+    type: "pie",
     data: {
-        labels: ['Done', 'Not Done'],
-        datasets: [{
-            label: '# of Votes',
-            data: [10, 5],
-            backgroundColor: [
-                'rgba(0, 255, 0, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-            ],
-        }]
-    }
+      labels: ["Done", "Not Done"],
+      datasets: [
+        {
+          label: "Successful checkins",
+          data: [response.length, 90 - response.length],
+          backgroundColor: ["rgba(0, 255, 0, 0.2)", "rgba(255, 99, 132, 0.2)"],
+        },
+      ],
+    },
+  });
 });
