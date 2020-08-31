02d5aaba10bc794dd268a99a636e504f297410c0
Dan Swan <rodger.swan@gmail.com>
2020-08-31 17:40:58 +1000

    checkin email feature

--- public/js/checkin.js
+++ public/js/checkin.js
@@ -3,28 +3,50 @@
 $("#checkin-button").click((event) => {
   event.preventDefault();
   let userId = localStorage.getItem("id");
-  let userEmail = localStorage.getItem("userEmail");
+  Swal.fire({
+    title: "Loading",
+    showCancelButton: false,
+    showConfirmButton: false,
+    buttonsStyling: true,
+    // html: "<pre><code>" + "You checked o" + "</code></pre>",
+    onBeforeOpen() {
+      Swal.showLoading();
+    },
+  }).then(function(result) {
+    if (result.value) {
+      //confirm
+      return;
+    }
+  });
   // Send the POST request.
-  console.log(userEmail);
   $.post("/api/checkin", {
     UserId: userId,
   })
     .then((data) => {
-   
-      //need login to disable check in button until date changed
-      // maybe display a handlebars that says "You've checked in your habit for today"
+      Swal.close();
+      Swal.fire({
+        title: "You checked in your habit today!",
+        type: "success",
+        showCancelButton: false,
+        showConfirmButton: true,
+        confirmButtonText: "Okay",
+        confirmButtonClass: "success",
+        buttonsStyling: true,
+        //  timer: 1500,
+        //  html: "<pre><code>" + info + "</code></pre>",
+      }).then(function(result) {
+        if (result.value) {
+          location.reload();
+          return;
+        }
+        if (result.dismiss === Swal.DismissReason.cancel) {
+          location.reload();
+          return;
+        }
+        
+      });
     })
     .catch((err) => {
       console.log(err);
     });
-    
-  $.post("/api/checkin-email", {
-    email: userEmail,
-  }).then(() => {
-    console.log(userEmail);
-
-  }).catch((err) => {
-    console.log(err)
-  });
-
 });
