ed51823509a4617f3258a2486bba1345e842f4c9
Nikki-Rees <nikki-rees@outlook.com>
2020-08-31 18:14:18 +1000

    update to config production

--- config/config.js
+++ config/config.js
@@ -16,11 +16,7 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: 3306,
+    use_env_variable: "JAWSDB_URL",
     dialect: "mysql"
   }
 };
