fdc279d763ba7a20774cb8d9ebef9fa98c42294d
Dan Swan <rodger.swan@gmail.com>
2020-08-31 17:42:35 +1000

    user habit refreshes on checkin page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,27 +1,25 @@
-/* eslint-disable */
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
-
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 const db = require("./../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../views/index.html"));
   });
 
   app.get("/habits", (req, res) => {
     db.Habit.findAll()
-      .then((habits) => {
-        habits = habits.map((habit) => {
+      .then(habits => {
+        habits = habits.map(habit => {
           return habit.dataValues;
         });
 
         res.render("habits", { habits });
       })
-      .catch((err) => console.log(err));
+      .catch(err => console.log(err));
   });
 
   app.get("/signup", (req, res) => {
@@ -46,16 +44,11 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 
+  app.get("/checkin", isAuthenticated, async (req, res) => {
+    const user = await db.User.findOne({ where: { id: req.user.id } });
 
-  app.get("/checkin", isAuthenticated, (req, res) => {
-    // console.log(req)
-    db.Habit.findOne({ where: { id: req.user.HabitId } })
-      .then((habit) => {
-        // console.log("HEY I'M TRIGGERING")
-        res.render("checkin", { habit: habit.dataValues });
-        // res.redirect("/checkin");
-      });
-
+    db.Habit.findOne({ where: { id: user.HabitId } }).then(habit => {
+      res.render("checkin", { habit: habit.dataValues });
+    });
   });
 };
-
